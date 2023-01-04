5551863a716f3b81bde1bd954d36364810471408
ahmedzaki98 <ahmedzaki19998@gmail.com>
2023-01-04 22:03:02 +0200

    Added Login & Logout functionality

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ import userRoute from "./routes/users.js";
 import postRoute from "./routes/posts.js";
 import commentRoute from "./routes/comments.js";
 import likeRoute from "./routes/likes.js";
+import cors from "cors";
+// import multer from "multer";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 //middleware
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
