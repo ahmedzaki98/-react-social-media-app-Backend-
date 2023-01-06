a8a703d252d01c4ff6e2267361dc51016f86fc14
ahmedzaki98 <ahmedzaki19998@gmail.com>
2023-01-06 17:15:16 +0200

    Added backend

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
 import express from "express";
-import authRoute from "./routes/auth.js";
-import userRoute from "./routes/users.js";
-import postRoute from "./routes/posts.js";
-import commentRoute from "./routes/comments.js";
-import likeRoute from "./routes/likes.js";
+const app = express();
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import commentRoutes from "./routes/comments.js";
+import likeRoutes from "./routes/likes.js";
+import relationshipRoutes from "./routes/relationships.js";
 import cors from "cors";
-// import multer from "multer";
+import multer from "multer";
 import cookieParser from "cookie-parser";
 
-const app = express();
-
-//middleware
+//middlewares
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(cookieParser());
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/public/upload");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
 
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/posts", postRoute);
-app.use("/api/comments", commentRoute);
-app.use("/api/likes", likeRoute);
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  res.status(200).json(file.filename);
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
+app.use("/api/relationships", relationshipRoutes);
 
 app.listen(8800, () => {
-  console.log("API WORKING!");
+  console.log("API working!");
 });
